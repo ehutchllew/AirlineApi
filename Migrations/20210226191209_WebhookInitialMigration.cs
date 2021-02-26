d91f622861191b3963707ed454452482192a31ab
Evan <ehutch7@gmail.com>
2021-02-26 14:18:50 -0500

    first db migration.

---
+++ Migrations/20210226191209_WebhookInitialMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirlineApi.Migrations
+{
+    public partial class WebhookInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WebhoookSubsriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WebhookURI = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Secret = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WebhookType = table.Column<int>(type: "int", nullable: false),
+                    WebhookPublisher = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebhoookSubsriptions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WebhoookSubsriptions");
+        }
+    }
+}
